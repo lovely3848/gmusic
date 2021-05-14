d1bb7beec6eb0f212bd75f620f0ed9059b0868a8
jaepil <lovely384832@gmail.com>
2021-05-14 18:55:20 +0900

    commit 0514

--- src/main/java/com/ncs/green/MemberController.java
+++ src/main/java/com/ncs/green/MemberController.java
@@ -68,7 +68,7 @@ public class MemberController {
 				service.pointChange(vo);
 				service.gradeChange(vo);
 				mv.setViewName("redirect:home");
-			} else {ss
+			} else {
 				mv.setViewName("payment/passbuy");
 			}
 		} else {
